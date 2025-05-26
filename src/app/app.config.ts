3064e07ba44f7ecf70464f525707010447ddad38
Sergio Mejia <sergio12mg25@gmail.com>
2025-05-26 11:06:24 -0500

    Se configura la redirección por scroll y las importaciones para usar PrimeNg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,34 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+// Importaciones de PrimeNG
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { MyPreset } from './mypreset';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled', // Restaura la posición del scroll al navegar hacia atrás/adelante
+        anchorScrolling: 'enabled', // Permite el scroll a elementos con IDs (fragmentos)
+      })
+    ),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: '.my-app-dark'
+        }
+      }
+    })
+  ]
 };
