5b73c03d4a348b2edfcd890c4cad93bc1a39a556
Sergio Mejia <sergio12mg25@gmail.com>
2025-05-26 11:07:03 -0500

    Archivos del componente princpal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import * as AOS from 'aos';
+
+// Componentes propios
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from "./intro/intro.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ButtonModule, ToolbarModule, HeaderComponent, IntroComponent, ProjectsComponent, AboutComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'portafolio';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    AOS.init({
+      duration: 800,
+      once: false,
+      offset: 100,
+      mirror: true
+    });
+  }
 }
