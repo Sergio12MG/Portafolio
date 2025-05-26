5988cd26ff6e709d05fbaf83e0911d5f75fd34f1
Sergio Mejia <sergio12mg25@gmail.com>
2025-05-26 11:05:26 -0500

    Configuración de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+    { path: '', component: IntroComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent }
+];
